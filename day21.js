68df2d864c7b8faeac9630ca8072d5503f7e9e7a
hello <hello@DESKTOP-G5MNAT4>
2021-02-16 08:34:52 +0530

    day 21

---
+++ day21.js
@@ -0,0 +1,25 @@
+// Type Script Example
+/* if you want to compile
+the ts file use tsc Compiler
+*/
+var i = 5;
+var x = "Day 21";
+console.log(i);
+i = "ashok";
+console.log(i + 'kumar');
+console.log(x);
+this.add();
+function add() {
+    var y = 25;
+    var z = 35;
+    console.log(y + z);
+    console.log(y > z);
+}
+console.log(i);
+var j = 25;
+var z;
+z = ++j;
+console.log(z);
+var sy = 5;
+var output = sy == 5 ? 'one' : 'two';
+console.log(output);
---
+++ day21.ts
@@ -0,0 +1,32 @@
+// Type Script Example
+
+ /* if you want to compile 
+the ts file use tsc Compiler
+*/
+ 
+var i:any =5;
+var x:string ="Day 21";
+console.log(i);
+i="ashok";
+console.log(i+'kumar')
+console.log(x);
+this.add();
+ function add(){
+	
+	var y:number =25
+	var z:number =35
+	console.log(y+z)
+	console.log(y >z)
+}
+console.log(i);
+
+var j:number=25;
+var z:number;
+z=++j;
+
+console.log(z)
+
+var sy:number=5;
+
+var  output = sy ==5 ?'one':'two';
+console.log(output)
