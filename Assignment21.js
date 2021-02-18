70d9ed9edbdc771a06b8b4340aa06a9e5e67fc39
samratsingh <samratsingh@localhost>
2021-02-17 20:09:06 -0500

    Assignment21

---
+++ Assignment21.js
@@ -0,0 +1,10 @@
+//state whether the below two lines of code is true or false;
+var x = 25;
+var x1 = x == 20 ? 'true' : 'false';
+console.log(x1);
+var xyz$uy = "Ashok";
+var x2 = xyz$uy == "Ashok" ? 'true' : 'false';
+console.log(x2);
+//Write a code to find factorial of 5
+var x = 5;
+console.log(x * 1 * 2 * 3 * 4);
---
+++ Assignment21.ts
@@ -0,0 +1,15 @@
+//state whether the below two lines of code is true or false;
+
+ var x:number =25;
+
+var x1 = x == 20 ? 'true':'false';
+console.log(x1)
+ 
+var xyz$uy : string="Ashok";
+var x2 = xyz$uy == "Ashok" ? 'true':'false';
+console.log(x2)
+
+
+//Write a code to find factorial of 5
+var x:number =5;
+console.log(x*1*2*3*4);
