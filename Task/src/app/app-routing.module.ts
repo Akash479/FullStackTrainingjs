5eeb975f01a122e5027b47fc4c94608ff522dce9
samratsingh <samratsingh@192.168.0.183>
2021-02-23 19:45:54 -0500

    Assignment Angular

---
+++ Task/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HelloComponent} from './hello/hello.component';
+
+const routes: Routes = [
+  {path: 'Hello', component: HelloComponent},
+  {path: '', redirectTo:'/hello-component', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
